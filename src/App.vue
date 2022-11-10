1d6a4569179470b19480e1d0462c1434b5292d14
yobrave <yobrave@outlook.com>
2022-11-10 17:55:36 +0800

    vite plugin: add unocss + unplugin-auto-import

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 const count = ref(5)
 
 const create = () => {
@@ -12,8 +11,8 @@ const cancel = () => {
 </script>
 
 <template>
-    <h2>Rectangle Creator 1111</h2>
-    <p>Count: <input v-model="count" /></p>
+    <h2 class="color-red">Rectangle Creator</h2>
+    <p class="p-10px">Count: <input v-model="count" /></p>
     <button @click="create">Create</button>
     <button @click="cancel">Cancel</button>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,34 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
+import Unocss from 'unocss/vite'
+import { presetAttributify, presetUno } from 'unocss'
+
+import AutoImport from 'unplugin-auto-import/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), viteSingleFile()],
+    plugins: [
+        Unocss({
+            mode: 'dist-chunk',
+            presets: [
+                presetAttributify(), 
+                presetUno()
+            ],
+            // shortcuts: {
+            //     'c-r': 'color-red'
+            // }
+        }),
+        AutoImport({
+            imports: [
+                // presets
+                'vue'
+            ],
+            dts: './src/auto-imports.d.ts'
+        }),
+        vue(),
+        viteSingleFile()
+    ],
     build: {
         // https://vitejs.cn/config/#build-csscodesplit
         cssCodeSplit: false,
